f2634c16d36de01ce64ddca37341d26a77e4ed0e
Wanderson Santos <wandersonsg10@gmail.com>
2024-06-13 21:04:29 -0300

    Criação dos componentes e contagem regressiva

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,37 @@
-console.log("Olá mundo!!");
+AOS.init();
+
+const dataDoEvento = new Date("Jan 11, 2025 00:01:00");
+const timesTampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function(){
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timesTampDoEvento - timeStampAtual;
+
+
+    const diasEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutoEmMs = 1000 * 60;
+
+
+
+    const diasAteOEvento =  Math.floor(distanciaAteOEvento / diasEmMs);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diasEmMs) / horaEmMs);
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % horaEmMs) / minutoEmMs);
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutoEmMs) / 1000);
+
+    console.log (distanciaAteOEvento);
+    console.log(horasAteOEvento);
+    console.log(minutosAteOEvento);
+    console.log(segundosAteOEvento);
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`
+
+    if (distanciaAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = 'Evento Expirado'
+    }
+
+
+},1000);
